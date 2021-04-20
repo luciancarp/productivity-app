78a31bd2911b87ee0a3e84564b1632425e978520
Lucian Carp <lucian.g.carp@gmail.com>
2021-04-20 21:38:51 +0300

    Add UserService tests for createAuthToken and loginUser

--- src/services/__tests__/user.ts
+++ src/services/__tests__/user.ts
@@ -2,13 +2,14 @@ import { Types } from 'mongoose'
 import User from '../../models/User'
 import UserService from '../user'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-describe('User Service', () => {
+describe('UserService', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  describe('when creating a user', () => {
+  describe('createUser', () => {
     const mockUser = {
       name: 'test',
       email: 'test@test.com',
@@ -52,100 +53,187 @@ describe('User Service', () => {
     })
   })
 
-  it('fetches a user by id', async () => {
-    const mockFetchedUser = {
-      _id: Types.ObjectId(),
-      name: 'test',
-      email: 'test@test.com',
-    }
+  describe('getUserById', () => {
+    it('fetches a user by id', async () => {
+      const mockFetchedUser = {
+        _id: Types.ObjectId(),
+        name: 'test',
+        email: 'test@test.com',
+      }
 
-    User.findById = jest.fn().mockImplementationOnce(() => ({
-      select: jest.fn().mockResolvedValueOnce(mockFetchedUser),
-    }))
+      User.findById = jest.fn().mockImplementationOnce(() => ({
+        select: jest.fn().mockResolvedValueOnce(mockFetchedUser),
+      }))
 
-    const fetchedUser = await UserService.getUserById(
-      mockFetchedUser._id.toString()
-    )
+      const fetchedUser = await UserService.getUserById(
+        mockFetchedUser._id.toString()
+      )
 
-    if (!fetchedUser) {
-      throw new Error('User is null')
-    }
+      if (!fetchedUser) {
+        throw new Error('User is null')
+      }
 
-    expect(fetchedUser.name).toEqual(mockFetchedUser.name)
-    expect(fetchedUser.email).toEqual(mockFetchedUser.email)
+      expect(fetchedUser.name).toEqual(mockFetchedUser.name)
+      expect(fetchedUser.email).toEqual(mockFetchedUser.email)
+    })
   })
 
-  it('fetches a user by email', async () => {
+  describe('getUserByEmail', () => {
+    it('fetches a user by email', async () => {
+      const mockUser = {
+        _id: Types.ObjectId(),
+        name: 'test',
+        email: 'test@test.com',
+      }
+
+      User.findOne = jest.fn().mockImplementationOnce(() => ({
+        select: jest.fn().mockResolvedValueOnce(mockUser),
+      }))
+
+      const fetchedUser = await UserService.getUserByEmail(mockUser.email)
+
+      if (!fetchedUser) {
+        throw new Error('User is null')
+      }
+
+      expect(fetchedUser.name).toEqual(mockUser.name)
+      expect(fetchedUser.email).toEqual(mockUser.email)
+    })
+  })
+
+  describe('updateUser', () => {
+    it('updates a user', () => {
+      const mockUser = {
+        _id: Types.ObjectId(),
+        name: 'test0',
+        email: 'test@test.com',
+        password: 'test',
+      }
+
+      const mockUpdatedUser = {
+        _id: mockUser._id,
+        name: 'test1',
+        email: 'test@test.com',
+        password: 'test',
+      }
+
+      const mockUpdate = {
+        name: 'test1',
+        email: 'test@test.com',
+        password: 'test',
+      }
+
+      const spy = jest
+        .spyOn(User, 'findOneAndUpdate')
+        .mockReturnValueOnce(mockUpdatedUser as any)
+
+      UserService.updateUser(mockUser._id.toString(), mockUpdate)
+
+      const spyUpdatedUser = spy.mock.results[0].value
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spyUpdatedUser).toEqual(mockUpdatedUser)
+    })
+  })
+
+  describe('deleteUser', () => {
+    it('deletes a user', () => {
+      const mockUser = {
+        _id: Types.ObjectId(),
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test',
+      }
+
+      const spy = jest
+        .spyOn(User, 'findOneAndDelete')
+        .mockReturnValueOnce(mockUser as any)
+
+      UserService.deleteUser(mockUser._id.toString())
+
+      const spyDeletedUser = spy.mock.results[0].value
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spyDeletedUser).toEqual(mockUser)
+    })
+  })
+
+  describe('createAuthToken', () => {
+    it('creates a token', async () => {
+      const mockId = Types.ObjectId()
+
+      const token = await UserService.createAuthToken(mockId.toString())
+
+      if (!token) {
+        throw new Error('Token is null')
+      }
+
+      let errorToken
+      let decodedToken
+      jwt.verify(token, process.env.JWT_SECRET || '', (error, decoded) => {
+        errorToken = error
+        decodedToken = decoded
+      })
+
+      expect(errorToken).toBeNull()
+      expect(decodedToken).toBeTruthy()
+    })
+  })
+
+  describe('loginUser', () => {
+    const mockLogin = {
+      email: 'test@test.com',
+      password: 'test',
+    }
+
     const mockUser = {
       _id: Types.ObjectId(),
       name: 'test',
       email: 'test@test.com',
-    }
-
-    User.findOne = jest.fn().mockImplementationOnce(() => ({
-      select: jest.fn().mockResolvedValueOnce(mockUser),
-    }))
-
-    const fetchedUser = await UserService.getUserByEmail(mockUser.email)
-
-    if (!fetchedUser) {
-      throw new Error('User is null')
-    }
-
-    expect(fetchedUser.name).toEqual(mockUser.name)
-    expect(fetchedUser.email).toEqual(mockUser.email)
-  })
-
-  it('updates a user', () => {
-    const mockUser = {
-      _id: Types.ObjectId(),
-      name: 'test0',
-      email: 'test@test.com',
       password: 'test',
     }
 
-    const mockUpdatedUser = {
-      _id: mockUser._id,
-      name: 'test1',
-      email: 'test@test.com',
-      password: 'test',
-    }
+    it('fetches user by email', async () => {
+      const spy = jest.spyOn(User, 'findOne').mockReturnValue(mockUser as any)
 
-    const mockUpdate = {
-      name: 'test1',
-      email: 'test@test.com',
-      password: 'test',
-    }
+      await UserService.loginUser(mockLogin.email, mockLogin.password)
 
-    const spy = jest
-      .spyOn(User, 'findOneAndUpdate')
-      .mockReturnValueOnce(mockUpdatedUser as any)
+      const spyFetchedUser = spy.mock.results[0].value
 
-    UserService.updateUser(mockUser._id.toString(), mockUpdate)
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spyFetchedUser.email).toEqual(mockUser.email)
+    })
 
-    const spyUpdatedUser = spy.mock.results[0].value
+    it('compares given password with stored hash', async () => {
+      const spy = jest.spyOn(bcrypt, 'compare').mockReturnValue(true as any)
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spyUpdatedUser).toEqual(mockUpdatedUser)
-  })
+      await UserService.loginUser(mockLogin.email, mockLogin.password)
 
-  it('deletes a user', () => {
-    const mockUser = {
-      _id: Types.ObjectId(),
-      name: 'test',
-      email: 'test@test.com',
-      password: 'test',
-    }
+      const spyIsMatch = spy.mock.results[0].value
 
-    const spy = jest
-      .spyOn(User, 'findOneAndDelete')
-      .mockReturnValueOnce(mockUser as any)
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spyIsMatch).toBeTruthy()
+    })
 
-    UserService.deleteUser(mockUser._id.toString())
+    it('returns token', async () => {
+      const token = await UserService.loginUser(
+        mockLogin.email,
+        mockLogin.password
+      )
 
-    const spyDeletedUser = spy.mock.results[0].value
+      if (!token) {
+        throw new Error('Token is null')
+      }
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spyDeletedUser).toEqual(mockUser)
+      let errorToken
+      let decodedToken
+      jwt.verify(token, process.env.JWT_SECRET || '', (error, decoded) => {
+        errorToken = error
+        decodedToken = decoded
+      })
+
+      expect(errorToken).toBeNull()
+      expect(decodedToken).toBeTruthy()
+    })
   })
 })
