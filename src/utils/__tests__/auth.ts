f4068ca12dd1b03ab15b784894924e02006296e5
Lucian Carp <lucian.g.carp@gmail.com>
2021-04-21 11:28:09 +0300

    Add utils/auth tests

---
+++ src/utils/__tests__/auth.ts
@@ -0,0 +1,90 @@
+import { Request, Response, NextFunction } from 'express'
+import { Types } from 'mongoose'
+import UserService from '../../services/user'
+import auth from '../auth'
+
+describe('Auth middleware', () => {
+  let mockRequest: Partial<Request>
+  let mockResponse: Partial<Response>
+  let nextFunction: NextFunction = jest.fn()
+
+  beforeEach(() => {
+    mockRequest = {}
+    mockResponse = {
+      status: jest.fn(),
+      json: jest.fn(),
+    }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('given a valid token', () => {
+    it('adds the user id to request', async () => {
+      const mockId = Types.ObjectId()
+
+      const token = await UserService.createAuthToken(mockId.toString())
+
+      if (!token) {
+        throw new Error('Token is undefined')
+      }
+
+      mockRequest = {
+        headers: {
+          'x-auth-token': token,
+        },
+      }
+
+      mockRequest.header = jest.fn().mockReturnValue(token)
+
+      await auth(mockRequest as Request, mockResponse as Response, nextFunction)
+
+      if (!mockRequest.user) {
+        throw new Error('mock request user is undefined')
+      }
+
+      expect(mockRequest.user.id).toEqual(mockId.toString())
+    })
+  })
+
+  describe('given an invalid token', () => {
+    it('returns 401 error', async () => {
+      const mockInvalidToken = 'test'
+
+      mockRequest = {
+        headers: {
+          'x-auth-token': mockInvalidToken,
+        },
+      }
+
+      const json = { errors: [{ msg: 'Token is not valid' }] }
+
+      mockRequest.header = jest.fn().mockReturnValue(mockInvalidToken)
+
+      await auth(mockRequest as Request, mockResponse as Response, nextFunction)
+
+      expect(mockResponse.status).toBeCalledWith(401)
+      expect(mockResponse.json).toBeCalledWith(json)
+    })
+  })
+
+  describe('given no token', () => {
+    it('returns 401 error', async () => {
+      mockRequest = {
+        headers: {
+          'x-auth-token': undefined,
+        },
+      }
+
+      const json = { errors: [{ msg: 'No token, authorization denied' }] }
+
+      mockRequest.header = jest.fn().mockReturnValue(undefined)
+
+      await auth(mockRequest as Request, mockResponse as Response, nextFunction)
+
+      expect(mockResponse.status).toBeCalledWith(401)
+      expect(mockResponse.json).toBeCalledWith(json)
+    })
+  })
+})
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
 
-const auth = async function (req: Request, res: Response, next: NextFunction) {
+const auth = async (req: Request, res: Response, next: NextFunction) => {
   // Get token from header
   const token = req.header('x-auth-token')
 
   // Check if no token
   if (!token) {
-    return res
-      .status(401)
-      .json({ errors: [{ msg: 'No token, authorization denied' }] })
+    res.status(401)
+    return res.json({ errors: [{ msg: 'No token, authorization denied' }] })
   }
 
   // Verify token
   try {
     jwt.verify(token, process.env.JWT_SECRET || '', (error, decoded) => {
       if (error) {
-        res.status(401).json({ errors: [{ msg: 'Token is not valid' }] })
+        res.status(401)
+        res.json({ errors: [{ msg: 'Token is not valid' }] })
       } else {
         req.user = (<any>decoded).user
         next()
@@ -24,7 +24,8 @@ const auth = async function (req: Request, res: Response, next: NextFunction) {
     })
   } catch (error) {
     console.error(`Could not verify token => ${error}`)
-    res.status(500).json({ errors: [{ msg: 'Server error' }] })
+    res.status(500)
+    res.json({ errors: [{ msg: 'Server error' }] })
   }
 }
 
